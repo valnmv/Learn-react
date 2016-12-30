f22734f621d02ed1ee33517183fd6bd914062ea8
Valentin Naumovm <vnaumov@yahoo.com>
2016-12-30 19:25:32 +0200

    added redux

--- app/main.js
+++ app/main.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import App from 'App';
-import About from 'About';
-import Home from 'Home';
+import createRoutes from 'routes';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-ReactDOM.render((
+const routes = createRoutes();
+
+ReactDOM.render(
   <MuiThemeProvider>
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Home}/>
-        <Route path="/about" component={About}/>
-      </Route>
-    </Router>
-  </MuiThemeProvider>), document.getElementById('root'));
+    <Provider>
+      <Router history={browserHistory}>
+        {routes}
+      </Router>
+    </Provider>
+  </MuiThemeProvider>,
+document.getElementById('root'));
---
+++ app/routes.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from 'App';
+import About from 'About';
+import Home from 'Home';
+
+export default () => {
+  return (
+    <Route path="/" component={App}>
+      <IndexRoute component={Home}/>
+      <Route path="/about" component={About}/>
+    </Route>
+  );
+};
