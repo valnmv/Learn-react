a30f1dddbcbc08fa8492988612d42ab05c9552c6
Valentin Naumovm <vnaumov@yahoo.com>
2016-12-30 11:24:30 +0200

    add material-ui

--- app/App.js
+++ app/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import styles from './App.css';
+import RaisedButton from 'material-ui/RaisedButton';
+// import styles from './App.css';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {test: 'foo'};
-  }
-  render() {
-    return (
-      <div className={styles.app}>
-        bar
-      </div>
-    );
-  }
-}
+const App = () => (
+  <RaisedButton label="Default" />
+);
+
+export default App;
--- app/main.js
+++ app/main.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './App.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+const MuiApp = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(<MuiApp />, document.getElementById('root'));
